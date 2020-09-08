9bd0ee676f99181a14b84caac5fde52466d31ad2
William Bagayoko <wbagayoko@gmail.com>
2020-09-08 11:57:08 -0500

    update homepage

--- ecommerce/src/App.vue
+++ ecommerce/src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <a href="questionnaire.html">Questionnaire</a>
-    <br />
-    <a href="ecommerce.html">Ecommerce</a>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="grid-container">
+      <div class="grid-row">
+        <div class="grid-offset-3 grid-col-6 margin-y-2">
+          <h1>Find your benefits</h1>
+          <ul class="usa-button-group">
+            <li class="usa-button-group__item">
+              <a
+                href="questionnaire.html"
+                class="usa-button usa-button--outline"
+              >Questionnaire Experience</a>
+            </li>
+            <li class="usa-button-group__item">
+              <a href="ecommerce.html" class="usa-button usa-button--outline">Ecommerce Experience</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
